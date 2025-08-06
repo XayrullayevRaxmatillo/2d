dfa5ef27bf7571702409f76a75dd4dbc25e2e194
XayrullayevRaxmatillo <xayrullayevrahmatilloh@gmail.com>
2025-08-06 16:33:55 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import './App.css'
+
+function App(){
+
+
+
+
+  return(
+    <div>
+       <header class="navbar">
+        <img src="./logo.png" alt="" />
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Resources</a></li>
+                <li><a href="#">Community</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Pricing</a></li>
+            </ul>
+        </nav>
+        <button class="btn">Register Now</button>
+    </header>
+
+    <section class="hero">
+        <div class="hero-text">
+            <h1>Lessons and insights <span>from 8 years</span></h1>
+            <p>Where to grow your business as a photographer: site or social media?</p>
+            <button class="btn">Register</button>
+        </div>
+        <div class="hero-img">
+            <img src="" alt="" />
+        </div>
+    </section>
+<div className="clients">
+        <h2>Our Clients</h2>
+        <div className="logos">
+            <img src="" alt="" />
+        </div>
+    </div>
+    
+    
+
+   
+    <section class="features">
+        <div class="card">
+            <h3>Membership Organizations</h3>
+            <p>Our membership management software provides full automation of membership renewals and payments.</p>
+        </div>
+        <div class="card">
+            <h3>National Associations</h3>
+            <p>Our membership management software provides full automation of membership renewals and payments.</p>
+        </div>
+        <div class="card">
+            <h3>Clubs and Groups</h3>
+            <p>Our membership management software provides full automation of membership renewals and payments.</p>
+        </div>
+    </section>
+
+    <section class="bottom">
+        <div class="bottom-img">
+           <img src="" alt="" />
+        </div>
+        <div class="bottom-text">
+            <h3>The unseen of spending three years at Pixelgrade</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <button class="btn">Learn More</button>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+export default App
